defeed36a6ceb800dbad128faf393f21688618ff
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2021-12-19 13:41:37 +0530

    Add files via upload

---
+++ Module 3/code8.c
@@ -0,0 +1,19 @@
+//Program to evaluate sum of 1/1 + 1/2 + 1/3 + ..+ 1/n
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    long double i, sum = 0.07;
+
+    printf("Program to evaluate sum of 1/1 + 1/2 + 1/3 + ..+ 1/n\n");
+    scanf("%d",&n);
+
+    for (i = 1; i <= n; i++){
+        sum = sum + 1/i;
+        }
+        
+    printf("Sum is %Lf\n", sum);
+    
+    return 0;
+}
