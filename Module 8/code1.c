6cb4f0b224bb28dddfe1adadf29c17faab4f5d3d
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2022-02-28 15:13:52 +0530

    Update code1.c

--- Module 8/code1.c
+++ Module 8/code1.c
@@ -1,20 +1,8 @@
 //Write a program to write a string in file
 #include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    FILE *fptr;
-    char str[80];
-    if ((fptr = fopen("test", "w")) == NULL)
-    {
-        printf("File does not exist\n");
-        exit(1);
-    }
-
-    printf("Enter Text: ");
-    while (gets(str) != NULL)
-        fputs(str, fptr);
-    fclose(fptr);
-    return 0;
+int main() {
+        FILE * fp;
+        fp = fopen("fputs_test.txt", "w+");
+        fputs("yo what's up!", fp);
+        return (0);
 }
