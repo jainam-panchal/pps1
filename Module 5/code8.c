aaf1c14d5b151b4325f16405eab5e801e54296b1
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2022-07-29 20:32:24 +0530

    Update code8.c

--- Module 5/code8.c
+++ Module 5/code8.c
@@ -1,21 +1,21 @@
 //Write a program to find sum of digits of the number using Recursive Function.
 #include <stdio.h>
-int add(int num)
+int fun(int n, int rem, int sum)
 {
-    if (num != 0)
-    {
-        return (num % 10 + add(num / 10));
-    }
+    if(n==0)
+        return sum;
     else
     {
-        return 0;
+        rem=n%10;
+        sum+=rem;
+        n/=10;
+        return fun(n, rem, sum);
     }
 }
-
 int main()
 {
-    int x, sum;
-    printf("Enter a number: ");
-    scanf("%d", &x);
-    printf("Sum of entered num: %d\n", add(x));
+    int n;
+    scanf("%d",&n);
+    printf("Sum of digits is %d",fun(n,0,0));
+    return 0;
 }
